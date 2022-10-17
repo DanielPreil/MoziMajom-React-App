6a51ea98b89c4807b67ba1d3b5454c3b527557f9
Daniel Preil <90137943+DanielPreil@users.noreply.github.com>
2022-10-17 22:00:34 +0200

    Data fetching működés képes NodeJs - server.js fájlból: server.js kommentek megírása a könyebb átláthatóság és kód megértése érdekében, App.js: Adatok lekérése server.js (api)-tól, ezek megszerzése után értékek kiíratása <p> tag ek közé a működés bemutatása érdekében egyelőre, kommentek szintén kiírva a megértés és az átlátás miatt, package.json (Folder: client): proxy: port: 5000 hozzáadva. React Frontend Működik + NodeJS Backend Alkalmazkodik és kommunikálnak. Következő lépés -> webscraping bevezetése és kiíratása.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+// How to Run React Frontend: (Terminal Commands)
+// -> cd client
+// -> npm start
+// -> Server ShutDown: CTRL + C, then Y (Yes) and Enter
 
+// Imports (React, sseEffect, useState) from react
+import React, { useEffect, useState } from 'react'
+
+// App
 const App = () => {
+
+  // Declare a new State Variable
+  const [backendData, setBackendData] = useState(({}))
+
+  // Fetch Backend Datat Once / loading the page from server.js
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  // App Frontend
+  // If backendData.users === undefined -> Get undefined
+  // Else get them inside <p> - tags
   return (
     <div>
-      <h1>App</h1>
+
+      {(typeof backendData.users === "undefined") ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
+// How to Run NodeJs Backend: (Terminal Commands)
+// -> cd server
+// -> npm run dev
+// -> Server ShutDown: CTRL + C, then Y (Yes) and Enter
+
+// Express Backend Server
 const express = require("express");
 const app = express()
 
+// Backend Data (EXAMPLE: USERNAMES)
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"] })
 })
 
+// App listening on the port of 5000 (Backend Server IS Online)
 app.listen(5000, () => {console.log("Server started on port 5000") })
